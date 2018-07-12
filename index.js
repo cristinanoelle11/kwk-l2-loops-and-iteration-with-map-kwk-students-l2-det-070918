fc9c3d67fc9bbbae2e4949f73caf803bf90318ed
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 17:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file.
 function lowerCaseStudentNames (list) {
- return list.map(function (driver) {
-  return driver.toLowerCase();
+ return list.map(function (student) {
+  return student.toLowerCase();
  });
 }
 
