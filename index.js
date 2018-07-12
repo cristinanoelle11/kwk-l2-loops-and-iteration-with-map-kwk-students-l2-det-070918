57b0a8c2f39c0d399acfc1761577437edbde8c51
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 17:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function lowerCaseStudentNames (list) {
 
 function nameToAttributes(list){
   return list.map(function(driver) {
-    const fName = driver.split('')[0];
-    const lName = driver.split('')[1];
+    const fName = driver.split(' ')[0];
+    const lName = driver.split(' ')[1];
     return {firstName: fName, lastName: lName};
   });
 }
